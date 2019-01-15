2cf1f0149c3106af3d56146b95983c53156d4cfd
Alex Posysoev <alekseyposysoev@gmail.com>
2019-01-15 19:51:15 +0300

    first commit

---
+++ ngApp/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialEventsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ server/routes/api.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+const db = 'mongodb://localhost:27017/angular-auth';
+
+mongoose.connect(db, {
+    useNewUrlParser: true
+}, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Connected to ${db}`);
+    }
+});
+
+router.get('/', (req, res) => {
+    res.send('from api router');
+});
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((err, registeredUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+
+    User.findOne({
+        email: userData.email
+    }, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email');
+            } else
+            if (user.password !== userData.password) {
+                res.status(401).send('Invalid password');
+            } else {
+                res.status(200).send(user);
+            }
+        }
+    });
+});
+
+router.get('/events', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+    ]
+    res.json(events);
+});
+
+router.get('/special', (req, res) => {
+    let events = [
+        {
+            "_id": "1",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "2",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "3",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "4",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id": "5",
+            "name": "Auto Expo",
+            "description": "lorem ipsum",
+            "date": "2012-04-23T18:25:43.511Z"
+        },
+    ]
+    res.json(events);
+});
+
+module.exports = router;
